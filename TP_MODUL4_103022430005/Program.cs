865a0c81866576fe32b80881868893ab821bee0d
Nasywa <nasywazahranailah@gmail.com>
2026-03-15 22:35:05 +0700

    second commit

--- TP_MODUL4_103022430005/Program.cs
+++ TP_MODUL4_103022430005/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kp = new KodePos();
+
+        Console.Write("Masukkan kelurahan: ");
+        string kel = Console.ReadLine();
+
+        Console.WriteLine("Kode Pos: " + kp.GetKodePos(kel));
+    }
+}
+
+class KodePos
+{
+    private Dictionary<string, string> kodePos = new Dictionary<string, string>()
+    {
+        {"Batununggal","40266"},
+        {"Kujangsari","40287"},
+        {"Mengger","40267"},
+        {"Wates","40256"},
+        {"Cijaura","40287"},
+        {"Jatisari","40286"},
+        {"Margasari","40286"},
+        {"Sekejati","40286"},
+        {"Kebonwaru","40272"},
+        {"Maleer","40274"},
+        {"Samoja","40273"}
+    };
+
+    public string GetKodePos(string kelurahan)
+    {
+        if (kodePos.ContainsKey(kelurahan))
+        {
+            return kodePos[kelurahan];
+        }
+        else
+        {
+            return "Kode pos tidak ditemukan";
+        }
+    }
+}
